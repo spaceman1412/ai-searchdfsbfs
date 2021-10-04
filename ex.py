d41af0ff74f2208848eee124d6a9c9b7cc6ef38f
spaceman1412 <spacemannormal@gmail.com>
2021-10-04 21:11:47 +0700

    Finish dfs

--- ex.py
+++ ex.py
@@ -66,17 +66,17 @@ def breadth_first_graph_search(problem):
     node = Node(problem.initial)
     if problem.goal_test(node.state):
         return node
-    frontier = deque([node])
+    frontier = deque()
+    frontier.append(node)
     explored = set()
     while frontier:
         node = frontier.popleft()
         explored.add(node.state)
         for child in node.expand(problem):
-            if child.state not in explored:
+            if child.state not in explored and child not in frontier:
                 if problem.goal_test(child.state):
                     return child
                 frontier.append(child)
-                explored.add(child.state)
 
     return None
 
@@ -92,19 +92,20 @@ def recursive(node, explored, problem):
 
 def depth_limited_search(problem, limit=50):
     """See [Figure 3.17] for the algorithm"""
-    # node = Node(problem.initial)
-    # frontier = deque([node])
-    # explored = set()
-    # while frontier:
-    #     node = frontier.popleft()
-    #     explored.add(node.state)
-    #     if problem.goal_test(node.state):
-    #         return node
-    #     for child in node.expand(problem):
-    #         if child.state not in explored:
-    #             frontier.append(child)
-    #             explored.add(child.state)
-    # return None
+    node = Node(problem.initial)
+    frontier = []
+    frontier.append(node)
+    explored = set()
+    while frontier:
+        node = frontier.pop()
+        explored.add(node.state)
+        if problem.goal_test(node.state):
+            return node
+        for child in node.expand(problem)[::-1]:
+            if child.state not in explored:
+                frontier.append(child)
+                explored.add(child.state)
+    return None
 
 
 ''' IMPLEMENT THE FOLLOWING FUNCTION '''
